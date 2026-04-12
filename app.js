c83c4d809fca9f63ce6332030b1a3c08c9d66448
nhicko-design <phan.anhtu1709@gmail.com>
2026-04-12 15:53:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+console.log("APP LOADED");
 import { renderRoute } from "./router.js";
 import { Navbar } from "./components/navbar.js";
 
 const app = document.getElementById("app");
-console.log("APP LOADED");
+
 function render() {
   app.innerHTML = `
     ${Navbar()}
